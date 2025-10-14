902aad5243e10e237ecafed8dcbc123242f642ca
DiegoN002 <diegonavao002@gmail.com>
2025-10-14 11:25:13 -0600

    v4.1.2

--- script.js
+++ script.js
@@ -707,7 +707,7 @@ local function setupFarmAvanzadaTab()
                 if npcMasCercano then
                     humanoidRootPart.CFrame = npcMasCercano.CFrame + Vector3.new(2, 0, 0)
                 end
-                wait(0.2)
+                wait(0)
             end)
         else
             print("🔴 TP auto DESACTIVADO")
